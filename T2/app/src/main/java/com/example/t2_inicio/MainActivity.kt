d2f4903a53bb74e460d77056ef87179ccdf6f614
Andrés López Zamarreño <lopez.zama@gmail.com>
2022-10-06 13:00:34 +0200

    update

--- T2/app/src/main/java/com/example/t2_inicio/MainActivity.kt
+++ T2/app/src/main/java/com/example/t2_inicio/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.t2_inicio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var botonPulsar: Button
+    lateinit var textoSaludo: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //xml + kt
+        botonPulsar = findViewById(R.id.botonPulsar)
+        textoSaludo = findViewById(R.id.textoBienvenida)
+
+        botonPulsar.setOnClickListener ({ if(textoSaludo.text.isEmpty()) textoSaludo.setText("Primera app completa")
+        else textoSaludo.setText("")})
+        //cuando pulse el boton que ponga un texto en el textview
+
     }
 }
