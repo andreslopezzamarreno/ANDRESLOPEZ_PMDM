d03d1f24833fdd09b623633ed1f03807886f0aef
Andrés López Zamarreño <lopez.zama@gmail.com>
2022-11-26 23:51:02 +0100

    update

--- Entregas/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Entregas/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -12,12 +12,18 @@ import com.example.calculadora.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var texto: String
+    var operacion:String? = ""
+    var op1: Double = 0.0
+    var op2: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        operacion = savedInstanceState?.getString("operacion")
+        op1 = savedInstanceState?.getDouble("op1")?:0.0
+        op2 = savedInstanceState?.getDouble("op2")?:0.0
+        binding.textNumeros.setText(savedInstanceState?.getString("texto",""))
         acciones()
     }
 
@@ -32,23 +38,26 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         binding.botonSiete.setOnClickListener(this)
         binding.botonOcho.setOnClickListener(this)
         binding.botonNueve.setOnClickListener(this)
+        binding.botonPunto.setOnClickListener(this)
         binding.botonAC.setOnClickListener(this)
-
         binding.botonMasMenos.setOnClickListener(this)
         binding.botonPorcentaje.setOnClickListener(this)
-        binding.botonDivision.setOnClickListener(this)
-        binding.botonMultiplicar.setOnClickListener(this)
-        binding.botonResta.setOnClickListener(this)
         binding.botonSuma.setOnClickListener(this)
+        binding.botonResta.setOnClickListener(this)
+        binding.botonMultiplicar.setOnClickListener(this)
+        binding.botonDivision.setOnClickListener(this)
+
         binding.botonIgual.setOnClickListener(this)
 
         binding.factorial?.setOnClickListener(this)
+        binding.raiz?.setOnClickListener(this)
+        binding.elev?.setOnClickListener(this)
+        binding.logaritmo?.setOnClickListener(this)
+        binding.e?.setOnClickListener(this)
 
     }
 
-    @SuppressLint("SetTextI18n")
     override fun onClick(p0: View?) {
-        texto = binding.textNumeros.text.toString()
         when (p0!!.id) {
             R.id.botonCero -> {
                 binding.textNumeros.append((p0 as Button).text)
@@ -80,16 +89,100 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             R.id.botonNueve -> {
                 binding.textNumeros.append((p0 as Button).text)
             }
+            R.id.botonPunto->{
+                binding.textNumeros.append((p0 as Button).text)
+            }
             R.id.botonAC->{
                 binding.textNumeros.text = ""
             }
-
-
-            /*R.id.botonMasMenos->{
-                
+            R.id.botonMasMenos->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+                binding.textNumeros.setText((op1 * -1).toString())
             }
+            R.id.botonPorcentaje->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+                binding.textNumeros.setText((op1 * 0.01).toString())
+            }
+            R.id.botonSuma->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+                binding.textNumeros.setText("")
+                operacion= "+"
+            }
+            R.id.botonResta->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+                binding.textNumeros.setText("")
+                operacion= "-"
+            }
+            R.id.botonMultiplicar->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+                binding.textNumeros.setText("")
+                operacion= "*"
+            }
+            R.id.botonDivision->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+                binding.textNumeros.setText("")
+                operacion= "/"
+            }
+            R.id.botonIgual->{
+                if (binding.textNumeros.text.isEmpty()){
+                    binding.textNumeros.setText(op1.toString())
+                }else {
+                    op2 = binding.textNumeros.text.toString().toDouble()
+                    when (operacion) {
+                        "+" -> {
+                            binding.textNumeros.setText((op1+op2).toString())
+                        }
+                        "-" -> {
+                            binding.textNumeros.setText((op1-op2).toString())
+                        }
+                        "*" -> {
+                            binding.textNumeros.setText((op1*op2).toString())
+                        }
+                        "/" -> {
+                            binding.textNumeros.setText((op1/op2).toString())
+                        }
+                        "elev"->{
+                            binding.textNumeros.setText((Math.pow(op1,op2)).toString())
+                        }
+                    }
+                }
+            }
+
+            R.id.raiz->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+
+                binding.textNumeros.setText((Math.sqrt(op1)).toString())
+            }
+            R.id.elev->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+                binding.textNumeros.setText("")
+                operacion = "elev"
+            }
+            R.id.logaritmo->{
+                op1 = binding.textNumeros.text.toString().toDouble()
+                binding.textNumeros.setText((Math.log(op1)).toString())
+            }
+            R.id.e->{
+                binding.textNumeros.setText((Math.E).toString())
+            }
+
             R.id.factorial->{
-            }*/
+                op1 = binding.textNumeros.text.toString().toDouble()
+                var factorial: Long = 1
+                println(op1.toInt())
+                for (i in 1..op1.toInt()) {
+                    factorial *= i.toLong()
+                }
+                binding.textNumeros.setText(factorial.toString())
+            }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("texto",binding.textNumeros.text.toString())
+        outState.putString("operacion",operacion)
+        outState.putDouble("op1",op1)
+        outState.putDouble("op2",op2)
     }
 }
